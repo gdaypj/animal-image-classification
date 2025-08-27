7f039dbfbd5a725477fdb194943a7ff0808ca58a
JPalita <ek20411@bristol.ac.uk>
2025-08-27 21:49:19 +0700

    fixing some bug

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import subprocess
+
+packages = str(subprocess.run('pip list', capture_output=True))
+st.markdown(packages.replace('\\r\\n', '  \\\n'))
 
 st.title("Image Classification Model: Cats and Dogs")
 
